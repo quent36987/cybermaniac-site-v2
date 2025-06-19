86645bd7a315acc0ec996ac45aedff20b271fecd
JulienTixier <tixier.julienfr@gmail.com>
2025-06-19 16:16:28 +0200

    maj site stagiaire Julien

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+
+
+export default ({
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        GrisFonce1 : "#171A26",
+        GrisFonce2 : "#161824",
+        GrisClair : "#262a3b",
+        Rose : "#F29BCB",
+        Violet : "#a759d9",
+      },
+    },
   },
   plugins: [],
-}
+});
